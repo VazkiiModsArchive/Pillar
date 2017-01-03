47761f61fb1a8e47e07382c31e940865b1b7a00d
Vasco Lavos <vazkii@hotmail.com>
2017-01-03 19:55:49 +0000

    Another IOUtils, yay #6

--- src/main/java/vazkii/pillar/command/CommandPillarCopy.java
+++ src/main/java/vazkii/pillar/command/CommandPillarCopy.java
@@ -19,7 +19,6 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
-import org.apache.commons.compress.utils.IOUtils;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.FileFilterUtils;
 import vazkii.pillar.Pillar;
